b9ba0c4ac6551299bd66ef1b5e328c271442b68d
Studio42-com <studio42dotcom@gmail.com>
2021-12-17 13:31:44 -0800

    revert again

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
-var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,15 +22,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-  }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
